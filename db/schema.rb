00ab7a074fc4c119d06262ec5de81db3bc1b7f71
Rails Udemy Course <duka.milan@gmail.com>
2017-01-09 17:27:30 +0000

    add password for user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108221354) do
+ActiveRecord::Schema.define(version: 20170109165449) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170108221354) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
