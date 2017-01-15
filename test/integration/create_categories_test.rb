28ab1910be12382d10017eeaab7ca9d03814ca4f
Rails Udemy Course <duka.milan@gmail.com>
2017-01-15 22:55:18 +0000

    add admin requirement to create categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
   test "get new category form and create category" do
     get new_category_path
     assert_template 'categories/new'
